c78c265b961e4a30e39ade663ffbeb60bc5e9b82
jirizverina <jiri.zverina01@gmail.com>
2024-12-07 20:12:09 +0100

    day 7 part 2

--- day7/src/main.zig
+++ day7/src/main.zig
@@ -1,12 +1,16 @@
 const std = @import("std");
 const expectEqual = std.testing.expectEqual;
+const math = std.math;
 
 pub fn main() !void {
     const allocator = std.heap.page_allocator;
     const parsed_input = try parseInput(allocator);
-    const result = getTotalCalibrationResult(parsed_input);
 
-    std.debug.print("Result is {}\n", .{result});
+    const result = getTotalCalibrationResult(parsed_input);
+    std.debug.print("Result for part 1 is {}\n", .{result});
+
+    const result2 = getTotalCalibrationResult2(parsed_input);
+    std.debug.print("Result for part 2 is {}\n", .{result2});
 }
 
 const CalibrationEquation = struct { result: u64, nums: []const u64 };
@@ -46,7 +50,7 @@ fn getTotalCalibrationResult(equations: []const CalibrationEquation) u64 {
     var sum: u64 = 0;
 
     for (equations) |equation| {
-        if (hasEquationSolution(equation)) {
+        if (addOrMultiplyNums(equation.nums[1..], equation.result, equation.nums[0])) {
             sum += equation.result;
         }
     }
@@ -54,8 +58,16 @@ fn getTotalCalibrationResult(equations: []const CalibrationEquation) u64 {
     return sum;
 }
 
-fn hasEquationSolution(equation: CalibrationEquation) bool {
-    return addOrMultiplyNums(equation.nums[1..], equation.result, equation.nums[0]);
+fn getTotalCalibrationResult2(equations: []const CalibrationEquation) u64 {
+    var sum: u64 = 0;
+
+    for (equations) |equation| {
+        if (addOrMultiplyOrConcatNums(equation.nums[1..], equation.result, equation.nums[0])) {
+            sum += equation.result;
+        }
+    }
+
+    return sum;
 }
 
 fn addOrMultiplyNums(nums: []const u64, expected_result: u64, curr_result: u64) bool {
@@ -74,11 +86,42 @@ fn addOrMultiplyNums(nums: []const u64, expected_result: u64, curr_result: u64) 
     return addOrMultiplyNums(nums[1..], expected_result, nums[0] + curr_result);
 }
 
+fn addOrMultiplyOrConcatNums(nums: []const u64, expected_result: u64, curr_result: u64) bool {
+    if (curr_result > expected_result) {
+        return false;
+    }
+
+    if (nums.len == 0) {
+        return curr_result == expected_result;
+    }
+
+    if (addOrMultiplyOrConcatNums(nums[1..], expected_result, nums[0] * curr_result)) {
+        return true;
+    }
+
+    if (addOrMultiplyOrConcatNums(nums[1..], expected_result, nums[0] + curr_result)) {
+        return true;
+    }
+
+    const num = nums[0];
+    const digits: u64 = math.log10_int(num) + 1;
+    const result = curr_result * math.pow(u64, 10, digits) + num;
+
+    return addOrMultiplyOrConcatNums(nums[1..], expected_result, result);
+}
+
 test "getTotalCalibrationResult" {
     const input = getTestInput();
     const result = getTotalCalibrationResult(input);
 
     try expectEqual(3749, result);
+}
+
+test "getTotalCalibrationResult2" {
+    const input = getTestInput();
+    const result = getTotalCalibrationResult2(input);
+
+    try expectEqual(11387, result);
 }
 
 inline fn getTestInput() []const CalibrationEquation {
